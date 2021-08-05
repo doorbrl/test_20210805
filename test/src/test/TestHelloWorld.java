cad6660e32ea5cc9618a54deeee9f8bd751c96db
ryo <ryohakama.kuc@gmail.com>
2021-08-05 16:44:38 +0900

    変更あり

--- test/src/test/TestHelloWorld.java
+++ test/src/test/TestHelloWorld.java
@@ -5,6 +5,9 @@ public class TestHelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World");
+		
+		System.out.println(4+8);
+		System.out.println("4+8");
 	}
 
 }
